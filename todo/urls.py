204bbea92d37fb52ad650817d4145fbea7eac2b7
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-08-29 23:22:09 +0300

    urls and views synched with update and delete

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, todo_create, todo_update
+from .views import home, todo_create, todo_delete, todo_update
 
 urlpatterns=[
     path('', home, name='home'),
-    path('add/', todo_create, name='add')
-    path('update/', todo_update, name='update')
+    path('add/', todo_create, name='add'),
+    path('update/<int:id>', todo_update, name='update'),
+    path('delete/<int:id>', todo_delete, name='delete')
 
 
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,40 +1,62 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
-
-# Create your views here.
 from django.http import HttpResponse
 from .models import Todo
 from .forms import TodoForm
-
+from django.contrib import messages
 
 def home(request):
-    todos=Todo.objects.all()
-    form=TodoForm()
-    context={
-        'todos':todos,
-        'form':form
+    todos = Todo.objects.all()
+    form = TodoForm()
+    context = {
+        "todos" : todos,
+        "form" : form
     }
-    return render(request, 'todo/home.html', context)
+
+    return render(request, "todo/home.html", context)
 
 def todo_create(request):
-    form=TodoForm()
-    if request.method =='POST':
-        form=TodoForm(request.POST)
+    form = TodoForm()
+
+    if request.method == "POST":
+        form = TodoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            messages.success(request,"Todo created successfully")
+            return redirect("home")
 
-    context={
-        'form' : form
+    context = {
+        "form" : form
     }
-    return render(request, 'todo/todo_add.html', context)
 
+    return render(request, "todo/todo_add.html", context)
 
 def todo_update(request, id):
-    todo = Todo.object.get(id=id)
+    todo = Todo.objects.get(id=id)
     form = TodoForm(instance=todo)
 
-    context={
-        'todo':todo,
-        'form': form
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {
+        "todo" : todo,
+        "form" : form
     }
-    return render(request, 'todo/todo_update', context)
+
+    return render(request, "todo/todo_update.html", context)
+
+def todo_delete(request, id):
+    todo = Todo.objects.get(id=id)
+
+    if request.method == "POST":
+        todo.delete()
+        messages.warning(request, "Todo deleted!")
+        return redirect("home")
+    context = {
+        "todo" : todo
+    }
+
+    return render(request, "todo/todo_delete.html", context)
